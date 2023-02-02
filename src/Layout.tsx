e335dd3a0a79f76ff118c82a9fed9f110125dbe6
Geanny Hernandez Rodriguez <geanny.hernandez@gmail.com>
2023-02-02 14:47:11 -0500

    adding basic layout with react router v6

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,5 +1,14 @@
+import React from "react";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+
 const Layout = () => {
-  return <div>Outlet</div>;
+  return (
+    <div>
+      <div>Navbar</div>
+      <div>News | Faves</div>
+      <Outlet />
+    </div>
+  );
 };
 
 export default Layout;
